2a1d7c7004fc4a763fd95f0c1e907473bb47c91c
Abubakar Kajiru <abubakarkajiru@outlook.com>
2019-03-04 10:13:52 +0300

    Added name, date released and artist; Added node_modules and package-lock

---
+++ Homework #1/song.js
@@ -0,0 +1,20 @@
+//Hi, the name's Kajiru; Abubakar Kajiru.
+//This is a JavaScript file that demonstrates the use of different data constructs to store and display information.
+/*The information in question in this case is of one of my favorite songs - 
+a simple, undeniably beautiful song, Just the Way You Are by Bruno Mars. */
+
+//Data Representation
+
+var name = "Just the Way You Are"
+var artist = "Bruno Mars"
+var dateReleased = {
+    day = 6,
+    month = 4,
+    year = 1996
+};
+
+//Data Display.
+
+Console.log("The name of the song is " + name + ".");
+Console.log("The Artist who created this masterpiece is known as " + artist + ".");
+Console.log("The Song was released on " + dateReleased.day + ", " + dateReleased.month + ", " + dateReleased.year + ".");
