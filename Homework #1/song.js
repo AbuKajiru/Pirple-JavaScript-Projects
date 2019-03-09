1bcc26e85b1cdaf22e63eee5fb83234f3603f381
Abubakar Kajiru <abubakarkajiru@outlook.com>
2019-03-09 14:50:22 +0300

    Completed project

--- Homework #1/song.js
+++ Homework #1/song.js
@@ -1,7 +1,7 @@
 //Hi, the name's Kajiru; Abubakar Kajiru.
 //This is a JavaScript file that demonstrates the use of different data constructs to store and display information.
 /*The information in question in this case is of one of my favorite songs - 
-a simple, undeniably beautiful song, Just the Way You Are by Bruno Mars. */
+a simple, undeniably beautiful song: "Just the Way You Are" by Bruno Mars. */
 
 //Data Representation
 
@@ -40,5 +40,5 @@ console.log("Songwriters: " + "\n- " + songwriters.join(". \n- ") + ".");
 console.log("Producers: " + producers.join(" and ") + ".");
 console.log("Production Labels: " + label.join(" . ") + ".");
 console.log("RECEPTION: \n" + "\t1. Nominations: \n" + "\t\t 2010: \n\t\t\t> " + reception.nominations[2010]  + "." + "\n\t\t 2011: \n\t\t\t> " 
-+ reception.nominations[2011].join(". \n\t\t\t> ") + "." + "\n\t2. Awards: \n" + "\t\t 2011: \n\t\t\t> " + reception.awards[2011].join(". \n\t\t\t> ") + "\n\t\t 2012: \n\t\t\t> " 
++ reception.nominations[2011].join(". \n\t\t\t> ") + "." + "\n\t2. Awards: \n" + "\t\t 2011: \n\t\t\t> " + reception.awards[2011].join(". \n\t\t\t> ") + "." + "\n\t\t 2012: \n\t\t\t> " 
 + reception.awards[2012].join(". \n\t\t\t> ") + ".");
